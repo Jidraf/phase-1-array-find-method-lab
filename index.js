9fd8d33698f7a1b306a755d34afe7db3a1470fa6
Jidraf Kimachia <raphjid170@gmail.com>
2022-06-17 09:18:10 +0300

    phase-1-array-find-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+
+// function superbowlWin(rec){
+//     for(const res of record){
+//         const ret = Object.values(res)
+//         console.log(ret[1])
+//         // return ret[1] === "W"
+//         // ret.find(W => {
+//         //     if(ret[1] === W) return true
+//         // })
+//     }
+//     // console.log(record.result)
+// }
+// // console.log(record.find(superbowlWin))
+// superbowlWin()
+
+const superbowlWin = (rec) => {
+    for(const item of rec){
+        if(item.result === "W"){
+            return item.year;
+        }
+    }
+    
+}
